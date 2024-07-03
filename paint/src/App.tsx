406c38da67bf91a1bc7483155d17ca192a2c66c7
Gabriel Duquette <gabrielduquette@gmail.com>
2024-07-04 00:25:59 +0100

    delete stuff that i moved elsewhere

--- paint/src/App.tsx
+++ paint/src/App.tsx
@@ -1,24 +1,15 @@
+import { Canvas } from "@react-three/fiber";
+
 import "./App.css";
 
-import { WebGLExample } from "./components/WebGL";
-import { ThreeJSExample } from "./components/ThreeJS";
-import { ReactThreeFiberExample } from "./components/ReactThreeFiber";
+import { Settings } from "./components/Settings";
 
 function App() {
   return (
-    <div className="comparison">
-      <div className="example">
-        <div className="label">webgl</div>
-        <WebGLExample />
-      </div>
-      <div className="example">
-        <div className="label">threejs</div>
-        <ThreeJSExample />
-      </div>
-      <div className="example">
-        <div className="label">react-three-fiber</div>
-        <ReactThreeFiberExample />
-      </div>
+    <div className="container">
+      <Canvas>
+        <Settings />
+      </Canvas>
     </div>
   );
 }
